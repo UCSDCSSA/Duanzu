3b812dc422a7c6ed0e577e81a28ffbeb27a46bc4
Liby Lee <liby99@icloud.com>
2017-11-11 12:37:45 -0800

    Fresh Start to Basic React App

---
+++ source/webpack.config.js
@@ -0,0 +1,19 @@
+var webpack = require('webpack')
+var path = require('path')
+module.exports = {
+  entry: path.resolve(__dirname, 'index'),
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'vendor.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']}
+    ]
+  }
+}
