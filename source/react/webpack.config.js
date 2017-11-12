9b043bf1463389c8833fa7eacde8b1fc243cf589
Liby Lee <liby99@icloud.com>
2017-11-11 17:26:05 -0800

    Changed Webpack Bundle Name

--- source/react/webpack.config.js
+++ source/react/webpack.config.js
@@ -1,19 +1,25 @@
 var webpack = require('webpack')
 var path = require('path')
 module.exports = {
-  entry: path.resolve(__dirname, 'index'),
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'vendor.js'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public')
-  },
-  module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']}
-    ]
-  }
+    entry: path.resolve(__dirname, 'index'),
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'webpack.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ['babel-loader']
+            }, {
+                test: /(\.css)$/,
+                loaders: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 }
