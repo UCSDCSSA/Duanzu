e922a2defe0b422655bf2e227f384cd434dba431
Liby Lee <liby99@icloud.com>
2017-11-11 17:17:30 -0800

    Move all front-end code to react folder

---
+++ source/react/components/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const HelloWorld = () => {
+    return (
+        <div className='hello-world'>
+            <h1>Hello World</h1>
+            <p>This is a good start</p>
+        </div>
+    )
+}
+export default HelloWorld
