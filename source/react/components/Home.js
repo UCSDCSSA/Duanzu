6a6a800ce41dcc78474088ac54b9f06139cd3ce6
Liby Lee <liby99@icloud.com>
2017-11-12 11:23:23 -0800

    Done Demo Home Page

--- source/react/components/Home.js
+++ source/react/components/Home.js
@@ -8,12 +8,50 @@ import { Link } from 'react-router-dom';
 
 import Header from './Header';
 
+class LeasingCard extends React.Component {
+    render () {
+        return (
+            <div className="col s4">
+                <div className="card">
+                    <img src="/img/cv.jpg" width="100%" />
+                    <center>
+                        <h5>{this.props.leasingName}</h5>
+                    </center>
+                </div>
+            </div>
+        );
+    }
+};
+
 const Home = () => {
+    
+    var allLeasingData = [
+        {
+            id: "_aoidsjfa9iq90irj 0iajsf",
+            name: "Costa Verde"
+        },
+        {
+            id: "_asjodfiaosifjao iajdsf",
+            name: "La Regencia"
+        },
+        {
+            id: "_asjodfiaosifjao iajdsf",
+            name: "International Garden"
+        }
+    ]
+    
+    var allLeasingElement = [];
+    for (var i = 0; i < allLeasingData.length; i++) {
+        allLeasingElement.push((<LeasingCard leasingId={allLeasingData[i].id} leasingName={allLeasingData[i].name}/>));
+    }
+    
     return (
         <div>
             <Header />
             <div className="container">
-                
+                <div className="row">
+                    {allLeasingElement}
+                </div>
             </div>
         </div>
     );
