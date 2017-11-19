42cff8ba98152c747091d0573355143cf5102a24
WenlinMao <maowenlin1997@gmail.com>
2017-11-19 11:43:28 -0800

    edit change profile and header

--- source/react/components/ChangeProfile.js
+++ source/react/components/ChangeProfile.js
@@ -4,37 +4,44 @@
  */
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-import { Card, Collection, CollectionItem, Row, Col, Input, Icon } from 'react-materialize';
+import {
+    Card,
+    Collection,
+    CollectionItem,
+    Row,
+    Col,
+    Input,
+    Icon
+} from 'react-materialize';
 
 import Header from './Header';
 
 class InfoCard extends React.Component {
-    render () {
+    render() {
 
         // TODO need info from data base
         return (
-
             <div>
                 <CollectionItem>
                     <Row>
                         <Col s={4}>
                             <center>
                                 <h5>
-                                    <Icon small left>{this.props.icon}</Icon>
+                                    <Icon small="small" left="left">{this.props.icon}</Icon>
                                     {this.props.infoName}
                                 </h5>
                             </center>
                         </Col>
-                        <Input s={7} label={this.props.infoID} />
+                        <Input s={7} label={this.props.infoID}/>
                     </Row>
                 </CollectionItem>
             </div>
         );
     }
 };
-//<i className="material-icons">keyboard_arrow_right</i>
+
 const ChangeProfile = () => {
 
     var allProfileInfo = [
@@ -42,18 +49,15 @@ const ChangeProfile = () => {
             id: "1",
             name: "Name",
             icon: "person"
-        },
-        {
+        }, {
             id: "2",
             name: "Email",
             icon: "email"
-        },
-        {
+        }, {
             id: "3",
             name: "Phone",
             icon: "phone"
-        },
-        {
+        }, {
             id: "4",
             name: "WeChat",
             icon: "chat"
@@ -62,17 +66,19 @@ const ChangeProfile = () => {
 
     var allProfileElement = [];
     //infoSite={allProfileInfo[i].infoLink}
-    for (var i = 0; i < allProfileInfo.length; i++)
-    {
-        allProfileElement.push((<InfoCard key={allProfileInfo[i].id}
-            infoID={allProfileInfo[i].id}
-            infoName={allProfileInfo[i].name}
-            icon={allProfileInfo[i].icon}/>));
+    for (var i = 0; i < allProfileInfo.length; i++) {
+        allProfileElement.push((
+            <InfoCard
+                key={allProfileInfo[i].id}
+                infoID={allProfileInfo[i].id}
+                infoName={allProfileInfo[i].name}
+                icon={allProfileInfo[i].icon}/>
+        ));
     }
 
-    return(
+    return (
         <div>
-            <Header />
+            <Header/>
             <Row>
                 <Col offset="l1" s={10}>
                     <div className="container">
