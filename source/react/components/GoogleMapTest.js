0a02e5778f08f97afe240abfb70fa9d0941d1be7
motwave <archer.yiyang@gmail.com>
2018-01-28 12:28:41 -0800

    rescaled map area

--- source/react/components/GoogleMapTest.js
+++ source/react/components/GoogleMapTest.js
@@ -6,15 +6,15 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 const GoogleMapTest = compose(
     withProps({
         googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCmglnfVoCJ5roESG_1ZdbbebdBnO5h3EY",
-        loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `400px` }} />,
-        mapElement: <div style={{ height: `100%` }} />,
+        loadingElement: <div style={{ height: `400%`, width: `50%` }} />,
+        containerElement: <div style={{ height: `185px` }} />,
+        mapElement: <div style={{ height: `400%`, width: `50%` }} />,
     }),
     withScriptjs,
-    withGoogleMap
+    withGoogleMap,
 )((props) =>
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 32.859823, lng: -117.228794 }}>
+        <Marker position={{ lat: 32.859823, lng: -117.228794 }} />
     </GoogleMap>
 )
 
