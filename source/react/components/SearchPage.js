91879678b7ccc783648c725b79e76f8ab1e866c3
ltyDion <til159@ucsd.edu>
2018-01-28 12:21:46 -0800

    searchPage add searchBar

--- source/react/components/SearchPage.js
+++ source/react/components/SearchPage.js
@@ -11,20 +11,83 @@ import {Input, Button, Card, Row, Col, Icon} from 'react-materialize';
 import Header from './Header';
 import LeasingCard from './LeasingCard';
 
+class Tag extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false
+        };
+    }
+
+
+    render () {
+        return (
+            <span style={
+                this.state.active ? {
+                    "backgroundColor": "rgba(0, 0, 255, 1)"
+                } : {}
+            } onClick={(e) => this.click(e)}>
+                <i className="material-icons">{this.props.icon}</i>
+                {this.props.text}
+            </span>
+        );
+    }
+
+    click () {
+        this.setState({
+            active: !this.state.active
+        });
+    }
+}
+
+class Tags extends React.Component {
+    render () {
+        var tags = [
+            {
+                "icon": "pets",
+                "text": "宠物"
+            },
+            {
+                "icon": "local_parking",
+                "text": "车位"
+            },
+            {
+                "icon": "local_laundry_service",
+                "text": "洗衣机"
+            },
+            {
+                "icon": "pool",
+                "text": "游泳池"
+            }
+
+        ];
+        var tagElements = [];
+        for (var i = 0; i < tags.length; i++) {
+            tagElements.push(<Tag key={i} icon={tags[i].icon} text={tags[i].text} />)
+        }
+        return (
+            <div>
+                {tagElements}
+            </div>
+        );
+    }
+}
+
 class SearchPage extends React.Component {
   render(){
     return(
-      <div>
+      <div className="search-page">
       <Header/>
-
+      <Tags />
     <div className="row">
       <div className="col l7 s12">
         <div id="SearchBar" className="input-field col s12">
 
           <Row>
-          <div className="col l2">
-              <Input placeholder="搜索房源" id="apartmentName" type="text" className="validate" >
-                <Icon style={{fontSize:'0.1 px'}}>insert_chart </Icon>
+          <div className="col l2 searchItem" >
+
+              <Input placeholder="搜索房源" id="apartmentName" type="text" className="validate">
+                  <Icon>account_circle</Icon>
               </Input>
           </div>
           <div className="col l4">
@@ -47,6 +110,13 @@ class SearchPage extends React.Component {
                 <Icon style={{fontSize:'0.1 px'}}>insert_chart </Icon>
               </Input>
           </div>
+          </Row>
+
+          <Row>
+            <div className="">
+
+            </div>
+
           </Row>
         </div>
 
@@ -96,6 +166,9 @@ class SearchPage extends React.Component {
     </div>
     );
   }
+  componentDidMount() {
 
+  }
 };
+
 export default SearchPage;
