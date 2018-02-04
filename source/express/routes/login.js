ff09428d5dc3b4cd2687655fd000b86775263b44
YUJIAN MI <miyujian@YUJIANs-MacBook-Pro.local>
2018-02-04 12:31:56 -0800

    hahaha

--- source/express/routes/login.js
+++ source/express/routes/login.js
@@ -1,8 +1,8 @@
 //require username and encoded password
 module.exports = function (req, res) {
-  if(req.body.username == NULL){
+  if(req.body.username == null){
     res.error(1);
-  }else if(req.body.password = NULL){
+  }else if(req.body.password = null){
     res.error(2);
   }
 }
