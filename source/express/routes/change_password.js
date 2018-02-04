38974c2d96bd6bb5aea9d71b7c63e2379cee73d0
ltyDion <til159@ucsd.edu>
2018-02-04 12:25:22 -0800

    try postman and back end test

--- source/express/routes/change_password.js
+++ source/express/routes/change_password.js
@@ -1,13 +1,13 @@
 //oldPassword,password
 module.exports = function (req, res) {
-  res.success("aaa");
-  // if(req.body.oldPassword == null){
-  //   res.error(2);
-  // }else if(req.body.newPassword == null){
-  //   res.error(3);
-  // }else if(req.body.confirmNewPassword == null){
-  //   res.error(4);
-  // }else if(req.body.newPassword != req.body.confirmNewPassword){
-  //   res.error(5);
-  // }
+  //res.success("aaa");
+  if(req.body.oldPassword == null){
+    res.error(2);
+  }else if(req.body.newPassword == null){
+    res.error(3);
+  }else if(req.body.confirmNewPassword == null){
+    res.error(4);
+  }else if(req.body.newPassword != req.body.confirmNewPassword){
+    res.error(5);
+  }
 }
