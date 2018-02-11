48c18f870c7774bb91818c017decf3b0c73bc5fe
YUJIAN MI <miyujian@YUJIANs-MacBook-Pro.local>
2018-02-11 12:56:49 -0800

    update change_password

--- source/express/routes/change_password.js
+++ source/express/routes/change_password.js
@@ -1,37 +1,5 @@
 //oldPassword,password
 module.exports = function (req, res) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< current
-  //res.success("aaa");
-  if(req.body.oldPassword == null){
-    res.error(2);
-  }else if(req.body.newPassword == null){
-    res.error(3);
-  }else if(req.body.confirmNewPassword == null){
-    res.error(4);
-  }else if(req.body.newPassword != req.body.confirmNewPassword){
-    res.error(5);
-  }
-=======
-  // res.success("aaa");
-  res.error(1000); 
-  // if(req.body.oldPassword == null){
-  //   res.error(2);
-  // }else if(req.body.newPassword == null){
-  //   res.error(3);
-  // }else if(req.body.confirmNewPassword == null){
-  //   res.error(4);
-  // }else if(req.body.newPassword != req.body.confirmNewPassword){
-  //   res.error(5);
-  // }
->>>>>>> before discard
-=======
-=======
->>>>>>> 97d66ce9c9b8d2d62bb60cdc241d1a1c49f8a139
-
-
-
   mongodb.user.findOne({
       "username": req.body.username
   }, function (err, result) {
@@ -54,8 +22,5 @@ module.exports = function (req, res) {
           }
       });
   })
-<<<<<<< HEAD
->>>>>>> 97d66ce9c9b8d2d62bb60cdc241d1a1c49f8a139
-=======
->>>>>>> 97d66ce9c9b8d2d62bb60cdc241d1a1c49f8a139
+
 }
