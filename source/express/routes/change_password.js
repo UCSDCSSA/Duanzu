f7b847451aa8d491b0484696e820883ca2e9af5e
Frank Zhang <zhangjie@zhangenqiqiqi.local>
2018-02-11 12:12:47 -0800

    wo yao pull la

--- source/express/routes/change_password.js
+++ source/express/routes/change_password.js
@@ -10,4 +10,14 @@ module.exports = function (req, res) {
   // }else if(req.body.newPassword != req.body.confirmNewPassword){
   //   res.error(5);
   // }
+  if(req.body.oldPassword == null){
+    res.error(2);
+  }else if(req.body.newPassword == null){
+    res.error(3);
+  }else if(req.body.confirmNewPassword == null){
+    res.error(4);
+  }else if(req.body.newPassword != req.body.confirmNewPassword){
+    res.error(5);
+  }
+  res.success("success")
 }
