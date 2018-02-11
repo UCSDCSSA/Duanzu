636e30a4d38a30a15abad5446a3fc3ff074b846f
Xinpei Tan <t347100955@gmail.com>
2018-02-11 13:00:50 -0800

    Updated change_profile.js

--- source/express/routes/change_profile.js
+++ source/express/routes/change_profile.js
@@ -1,23 +1,39 @@
 //username, picture, phone
 module.exports = function (req, res) {
-  res.success("aaa");
-  // if(req.body.oldPassword == null){
-  //   res.error(2);
-  // }else if(req.body.newPassword == null){
-  //   res.error(3);
-  // }else if(req.body.confirmNewPassword == null){
-  //   res.error(4);
-  // }else if(req.body.newPassword != req.body.confirmNewPassword){
-  //   res.error(5);
-  // }
-  //res.success("aaa");
-  if(req.body.oldPassword == null){
-    res.error(2);
-  }else if(req.body.newPassword == null){
-    res.error(3);
-  }else if(req.body.confirmNewPassword == null){
-    res.error(4);
-  }else if(req.body.newPassword != req.body.confirmNewPassword){
-    res.error(5);
-  }
+
+  mongodb.user.findProfile({
+  }, function (err, result) {
+    // if userid can not be found
+    if (err) {
+      res.error(101);
+    }
+    else {
+      mongodb.user.checkPicture({
+          "username": req.body.username
+          // "picture" : req.body.picture
+      }, function (err, result) {
+        // if picture failed to be uploaded
+        if (err) {
+          res.error(110);
+        }
+        else {
+          mongodb.collection("user").updateProfile({
+              "username": req.body.username
+          }, {
+              "picture" : req.body.picture
+              //"phone"   : req.body.phone
+          }, function (err, result) {
+            // if Profile failed to be modified
+            if (err) {
+              res.error(111);
+            }
+            else {
+              // Successfully changed profile, return username
+              res.success(req.body.username);
+            }
+          } );
+        }
+      } );
+    }
+  } );
 }
