bacc55fb3bb8637d1c78d640bcae4ec624187acf
EnqiZhang99 <enz004@ucsd.edu>
2018-02-25 12:59:42 -0800

    Check email

--- source/express/check_email.js
+++ source/express/check_email.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-function is_email_used(){
-    var arg = Array.toArray(arguments);
-    
-=======
 module.exports = {
-    "is_email_used": function (){
-        return false;
+    "is_email_used": function (email, callback, error){
+        
+    },
+    "is_email_legal": function (email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
     }
-    "is_email_legal": function () {
-        return false;
-    }
-    
-    function is_email_
->>>>>>> 7004212df22272bb05594fab2f2f5a6a14713336
 }
