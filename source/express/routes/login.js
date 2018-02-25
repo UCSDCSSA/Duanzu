a7c9adbe7219abd93d3f6f363cd6bf29e4c0557b
qidian99 <qidian990107@163.com>
2018-02-26 02:32:34 +0800

    resolved conflicts

--- source/express/routes/login.js
+++ source/express/routes/login.js
@@ -2,7 +2,9 @@
 module.exports = function (req, res) {
   if(req.body.username == null){
     res.error(1);
-  }else if(req.body.password = null){
+
+}else if(req.body.password == null){
+
     res.error(2);
 } else {
     res.success("haha");
