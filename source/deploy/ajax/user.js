90f9eda8218909399b44b00d9269d1033f38d854
qidian99 <qidian990107@163.com>
2018-04-30 03:45:50 +0800

    added register function

--- source/deploy/ajax/user.js
+++ source/deploy/ajax/user.js
@@ -10,12 +10,25 @@ module.exports = {
             }
             else {
                 res.success(result);
+                // console.log(Crypto.genEncrypted("12345678"));
+                // console.log(Crypto.match("12345678", Crypto.genEncrypted("12345678")));
             }
         });
+    },
+
+    register: function (req, res) {
+        User.insertOne({"username": req.body.username,
+                        "email": req.body.email,
+                        "password": Crypto.genEncrypted(req.body.password) });
+
+        User.find({"username":req.body.username}).toArray(function (err, result) {
+            if (err) {
+                res.error(1, err);
+            }
+            else {
+                res.success(result);
+            }
+        });
+
     }
-
-
 }
-
-Crypto.genEncrypted("12345678");
-Crypto.match("12345677", "183u54428u50ytugw90ejr9sgij");
