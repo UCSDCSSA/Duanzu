f0f4b1353b4857fadae68b35f61e4ce31105a49d
Lola Liu <lola@Lolas-MacBook-Pro.local>
2018-04-29 16:46:11 -0700

    temp

--- source/react/components/Home.js
+++ source/react/components/Home.js
@@ -19,14 +19,16 @@ const Home = () => {
     };
 
     $(document).ready(function(){
-    $('.carousel').carousel();
+        $('.carousel carousel-slider').carousel({
+            fullWidth: true
+        });
     });
 
 
     return (
         <div className= "home-page">
             <Header/>
-            <div className="carousel">
+            <div className="carousel carousel-slider">
                 <a className="carousel-item" href="#one!"><img src="/img/cover_image.jpg"/></a>
                 <a className="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"/></a>
                 <a className="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"/></a>
