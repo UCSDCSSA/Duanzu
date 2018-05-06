7fb96756b421cbd89f7571a8933ef5638fdd8d17
Wenlin Mao <w6mao@ucsd.edu>
2018-05-06 12:31:59 -0700

    add removeAllUsers

--- source/deploy/ajax/user.js
+++ source/deploy/ajax/user.js
@@ -40,41 +40,48 @@ module.exports = {
     changePassword: function(req, res) {
         var currentPassword = req.body["password"];
         var newPassword = req.body["new_password"];
-        if (req.body["username"] && currentPassword && newPassword){
-            User.find({"username": req.body["username"]}).toArray(function (err, result) {
-                if (err) {
-                    res.error(1, err);
-                }
-                else
-                {
-                    if (result.length == 0){
-                        res.error(5, "User not found");
+        var confirmPassword = req.body["confirm_password"];
+        if (req.body["username"] && currentPassword && newPassword && confirmPassword){
+            if (newPassword != confirmPassword)
+            {
+                res.error(8, "confirm password is not same as new password");
+            }
+            else{
+                User.find({"username": req.body["username"]}).toArray(function (err, result) {
+                    if (err) {
+                        res.error(1, err);
                     }
-                    else {
-                        var user = result[0];
-                        if (Crypto.match(currentPassword, user["password"])) {
-                            res.error(6, "Password not match");
+                    else
+                    {
+                        if (result.length == 0){
+                            res.error(5, "User not found");
                         }
                         else {
-                            var hashNewPassword = Crypto.genEncrypted(newPassword);
-                            User.update({
-                                "_id": ObjectId(user["_id"])
-                            }, {
-                                "$set": {
-                                    "password": hashNewPassword
-                                }
-                            }, function (err, result) {
-                                if (err) {
-                                    res.error(7, err);
-                                }
-                                else {
-                                    res.success(result);
-                                }
-                            });
+                            var user = result[0];
+                            if (Crypto.match(currentPassword, user["password"])) {
+                                res.error(6, "Password not match");
+                            }
+                            else {
+                                var hashNewPassword = Crypto.genEncrypted(newPassword);
+                                User.update({
+                                    "_id": ObjectId(user["_id"])
+                                }, {
+                                    "$set": {
+                                        "password": hashNewPassword
+                                    }
+                                }, function (err, result) {
+                                    if (err) {
+                                        res.error(7, err);
+                                    }
+                                    else {
+                                        res.success(result);
+                                    }
+                                });
+                            }
                         }
                     }
-                }
-            });
+                });
+            }
         }
         else if (!req.body["username"]){
             res.error(2, "No username");
@@ -85,12 +92,17 @@ module.exports = {
         else if (!newPassword){
             res.error(4, "No new password");
         }
+        else if (!confirmPassword){
+            res.error(7, "No confirm password");
+        }
 
     },
     register: function (req, res) {
+        var username = req.body["username"];
+
         User.insertOne({
-            "username": req.body.username,
-            "email": req.body.email,
+            "username": req.body["username"],
+            "email": req.body["email"],
             "password": Crypto.genEncrypted(req.body.password)
         }, function (err, response) {
             if (err) {
@@ -100,5 +112,22 @@ module.exports = {
                 res.success(response.ops[0]);
             }
         });
-    }
+    },
+    removeAllUsers: function (req, res) {
+        if (User.drop())
+        {
+            res.success("drop success");
+        }
+        {
+            res.error(1, "collection does not exist");
+        }
+        // User.remove({}, false).toArray(function(err, result){
+        //     if (err){
+        //         res.error(1, err);
+        //     }
+        //     else {
+        //         res.success(result);
+        //     }
+        // });
+    },
 }
