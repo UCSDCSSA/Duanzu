bb36a0d3fb28108efcd03bd18afa3d4e551423ef
Liby Lee <liby99@icloud.com>
2018-05-06 10:28:08 -0700

    Attempting to add about

---
+++ source/deploy/ajax/about.js
@@ -0,0 +1,11 @@
+module.exports = {
+    getMembers (req, res) {
+        res.success([
+            {
+                "name": "李子阳"
+            }, {
+                "name": "糜雨健"
+            }
+        ]);
+    }
+}
